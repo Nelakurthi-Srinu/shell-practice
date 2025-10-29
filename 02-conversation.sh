c899050d5f22ccd9999b7553cca1db32781eae1a
Nelakurthi Srinu <srinu.nelakurthi@gmail.com>
2025-10-29 23:36:20 +0530

    created variables.sh

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Sachin:: Hi Rahul, How are you?"
+echo "Rahul:: Hi Sachin I am fine. How are you?"
+echo "Sachin:: What are you learning?"
+echo "Rahul:: I am learning shell scripting Sachin"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSION1=Sachin
+PERSION2=Rahul
+
+echo "$PERSION1:: Hi $PERSION2, How are you?"
+echo "$PERSION2:: Hi $PERSION1 I am fine. How are you?"
+echo "$PERSION1:: What are you learning?"
+echo "$PERSION2:: I am learning shell scripting $PERSION1"
